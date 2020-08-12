1c14027ed143bc0c8f04bdc2981310a9d6178e37
benjaminrimur <ben.lim80@yahoo.com.sg>
2020-08-12 11:52:21 +0800

    commit

--- main_page.php
+++ main_page.php
@@ -204,8 +204,8 @@
                 </div>
                 <!-- About Section Content-->
                 <div class="row">
-                    <div class="col-lg-4 ml-auto"><p class="lead">Freelancer is a free bootstrap theme created by Start Bootstrap. The download includes the complete source files including HTML, CSS, and JavaScript as well as optional SASS stylesheets for easy customization.</p></div>
-                    <div class="col-lg-4 mr-auto"><p class="lead">You can create your own custom avatar for the masthead, change the icon in the dividers, and add your email address to the contact form to make it fully functional!</p></div>
+                    <div class="col-lg-4 ml-auto"><p class="lead">We are a group of students from Singapore Polytechnic trying to make a website to develop solutions to help the elderly to lead active and dignified lives in the silver years.</p></div>
+                    <div class="col-lg-4 mr-auto"><p class="lead">We are located at 500 Dover Rd, Singapore 139651</p></div>
                 </div>
                 <!-- About Section Button-->
                 <div class="text-center mt-4">
