7396ebc7a5a90c712a9cdfec436393f907b7739e
benjaminrimur <ben.lim80@yahoo.com.sg>
2020-08-12 11:40:25 +0800

    commit

--- main_page.php
+++ main_page.php
@@ -481,7 +481,7 @@
                                     <!-- Portfolio Modal - Image-->
                                     <img class="img-fluid rounded mb-5" src="assets/img/rewards/grocery_3.jpg" alt="" />
                                     <!-- Portfolio Modal - Text-->
-                                    <p class="mb-5">Text</p>
+                                    <p class="mb-5">Click proceed to find more on the groceries you can redeem</p>
                                     <a href="rewards/grocery_products.php" class="btn btn-primary"><i class="fas fa-arrow-circle-right"></i> Proceed</a>
                                 </div>
                             </div>
@@ -512,7 +512,7 @@
                                     <!-- Portfolio Modal - Image-->
                                     <img class="img-fluid rounded mb-5" src="assets/img/rewards/health_2.jpg" alt="" />
                                     <!-- Portfolio Modal - Text-->
-                                    <p class="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
+                                    <p class="mb-5">Click proceed to find more on the health products you can redeem</p>
                                     <a href="rewards/health_products.php" class="btn btn-primary"><i class="fas fa-arrow-circle-right"></i> Proceed</a>
                                 </div>
                             </div>
@@ -543,7 +543,7 @@
                                     <!-- Portfolio Modal - Image-->
                                     <img class="img-fluid rounded mb-5" src="assets/img/rewards/home.jpg" alt="" />
                                     <!-- Portfolio Modal - Text-->
-                                    <p class="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
+                                    <p class="mb-5">Click proceed to find more on the home appliances you can redeem</p>
                                     <a href="rewards/home_products.php" class="btn btn-primary"><i class="fas fa-arrow-circle-right"></i> Proceed</a>
                                 </div>
                             </div>
